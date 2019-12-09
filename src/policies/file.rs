3932a31fe148b8caf5f0e6696f0af0d7374bf12e
Charles Bassett <chasb96@gmail.com>
2019-12-08 18:01:26 -0800

    Added tests around user restriction

--- src/policies/file.rs
+++ src/policies/file.rs
@@ -1,9 +1,12 @@
 use super::Restrict;
 use entities::models::{File, User};
+use crate::services::folder::FolderService;
 
 impl Restrict<User> for File {
     fn viewable_by(&self, user: &User) -> bool {
-        let folder = match self.folder() {
+        let folder_service = resolve!(FolderService);
+
+        let folder = match folder_service.find(self.folder_id()) {
             Ok(folder) => folder,
             Err(_) => return false,
         };
@@ -12,7 +15,9 @@ impl Restrict<User> for File {
     }
 
     fn modifiable_by(&self, user: &User) -> bool {
-        let folder = match self.folder() {
+        let folder_service = resolve!(FolderService);
+
+        let folder = match folder_service.find(self.folder_id()) {
             Ok(folder) => folder,
             Err(_) => return false,
         };
@@ -21,7 +26,9 @@ impl Restrict<User> for File {
     }
 
     fn deletable_by(&self, user: &User) -> bool {
-        let folder = match self.folder() {
+        let folder_service = resolve!(FolderService);
+
+        let folder = match folder_service.find(self.folder_id()) {
             Ok(folder) => folder,
             Err(_) => return false,
         };
--- src/policies/user.rs
+++ src/policies/user.rs
@@ -24,3 +24,159 @@ impl Restrict<User> for User {
         user.is_admin() || self.id() == user.id()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use entities::models::User;
+    use crate::entities::builders::{ Builder, UserBuilder };
+    use policies::Restricted;
+
+    #[test]
+    fn test_indexable_by_admin() {
+        let mut admin = factory!(User);
+        admin.set_role("admin".to_string());
+
+        assert!(admin.can_index::<User>());
+    }
+
+    #[test]
+    fn test_indexable_by_non_admin() {
+        let mut user = factory!(User);
+        let mut guest = factory!(User);
+        user.set_role("user".to_string());
+        guest.set_role("guest".to_string());
+
+        assert!(!user.can_index::<User>());
+        assert!(!guest.can_index::<User>());
+    }
+
+    #[test]
+    fn test_createable_by_admin() {
+        let mut admin = factory!(User);
+        admin.set_role("admin".to_string());
+
+        assert!(admin.can_create::<User>());
+    }
+
+    #[test]
+    fn test_createable_by_non_admin() {
+        let mut user = factory!(User);
+        let mut guest = factory!(User);
+        user.set_role("user".to_string());
+        guest.set_role("guest".to_string());
+
+        assert!(!user.can_create::<User>());
+        assert!(!guest.can_create::<User>());
+    }
+
+    #[test]
+    fn test_viewable_by_admin() {
+        let mut admin = factory!(User);
+        let mut other = factory!(User);
+        admin.set_role("admin".to_string());
+        admin.set_id(2);
+        other.set_id(1);
+
+        assert!(admin.can_view(other));
+    }
+
+    #[test]
+    fn test_viewable_by_non_admin() {
+        let mut user = factory!(User);
+        let mut guest = factory!(User);
+        let mut other = factory!(User);
+        user.set_role("user".to_string());
+        guest.set_role("guest".to_string());
+        user.set_id(2);
+        guest.set_id(3);
+        other.set_id(1);
+
+        assert!(!user.can_view(other.clone()));
+        assert!(!guest.can_view(other));
+    }
+
+    #[test]
+    fn test_viewable_by_self() {
+        let mut user = factory!(User);
+        let mut other = factory!(User);
+        user.set_role("user".to_string());
+        user.set_id(1);
+        other.set_id(1);
+
+        assert!(user.can_view(other));
+    }
+
+    #[test]
+    fn test_modifiable_by_admin() {
+        let mut admin = factory!(User);
+        let mut other = factory!(User);
+        admin.set_role("admin".to_string());
+        admin.set_id(2);
+        other.set_id(1);
+
+        assert!(admin.can_modify(other));
+    }
+
+    #[test]
+    fn test_modifiable_by_non_admin() {
+        let mut user = factory!(User);
+        let mut guest = factory!(User);
+        let mut other = factory!(User);
+        user.set_role("user".to_string());
+        guest.set_role("guest".to_string());
+        user.set_id(2);
+        guest.set_id(3);
+        other.set_id(1);
+
+        assert!(!user.can_modify(other.clone()));
+        assert!(!guest.can_modify(other));
+    }
+
+    #[test]
+    fn test_modifiable_by_self() {
+        let mut user = factory!(User);
+        let mut other = factory!(User);
+        user.set_role("user".to_string());
+        user.set_id(1);
+        other.set_id(1);
+
+        assert!(user.can_modify(other));
+    }
+
+    #[test]
+    fn test_deletable_by_admin() {
+        let mut admin = factory!(User);
+        let mut other = factory!(User);
+        admin.set_role("admin".to_string());
+        admin.set_id(2);
+        other.set_id(1);
+
+        assert!(admin.can_delete(other));
+    }
+
+    #[test]
+    fn test_deletable_by_non_admin() {
+        let mut user = factory!(User);
+        let mut guest = factory!(User);
+        let mut other = factory!(User);
+        user.set_role("user".to_string());
+        guest.set_role("guest".to_string());
+        user.set_id(2);
+        guest.set_id(3);
+        other.set_id(1);
+
+        assert!(!user.can_delete(other.clone()));
+        assert!(!guest.can_delete(other));
+    }
+
+    #[test]
+    fn test_deletable_by_self() {
+        let mut user = factory!(User);
+        let mut other = factory!(User);
+        user.set_role("user".to_string());
+        user.set_id(1);
+        other.set_id(1);
+
+        assert!(user.can_delete(other));
+    }
+}
